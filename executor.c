05d591b19d237c2051e8594318fe0257a83de3f0
Toastuy <55900660+Toastuy@users.noreply.github.com>
2021-12-04 20:15:35 -0500

    more process node stuff wow

--- executor.c
+++ executor.c
@@ -18,16 +18,20 @@ void control_node(int rank, int num_procs) {
 
 void process_node(int rank, int num_procs) {
     transform_t *buffer;
-    int size;
+    int size, rank, num_procs;
+
+    // Get our MPI stuff
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
     // Get our data to work on
-    receive_data(buffer, size);
+    receive_data(buffer, size, rank, num_procs);
 
     // Change the data in place
     execute_workflow(buffer, size);
 
     // Send data over to control node
-    send_data(buffer, size);
+    send_data(buffer, size, rank, num_procs);
 
     // Free our buffer since we sent it and don't need it anymore
     free(buffer);
